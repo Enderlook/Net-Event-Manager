83d03e1b50bd592730f37428016c1a30c87abcd3
Enderlook <enderlook@gmail.com>
2021-04-11 12:04:54 -0300

    Remove unused code

--- Enderlook.EventManager/src/Closure/ClosureDelegate.cs
+++ Enderlook.EventManager/src/Closure/ClosureDelegate.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
 namespace Enderlook.EventManager
@@ -15,25 +13,6 @@ namespace Enderlook.EventManager
         {
             this.@delegate = @delegate;
             this.closure = closure;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public bool Equals(in ClosureDelegate<TClosure> other)
-            => @delegate.Equals(other.@delegate) && EqualityComparer<TClosure>.Default.Equals(closure, other.closure);
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Invoke<TEvent>(TEvent argument)
-        {
-            if (typeof(TEvent) == typeof(HasNoParameter))
-            {
-                Debug.Assert(@delegate is Action<TClosure>);
-                Unsafe.As<Action<TClosure>>(@delegate)(closure);
-            }
-            else
-            {
-                Debug.Assert(@delegate is Action<TClosure, TEvent>);
-                Unsafe.As<Action<TClosure, TEvent>>(@delegate)(closure, argument);
-            }
         }
     }
 }
