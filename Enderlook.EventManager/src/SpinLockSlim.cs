818785928e9f97641db73682c0200d002b5b7e3b
Enderlook <enderlook@gmail.com>
2024-04-18 23:22:21 -0300

    .

--- Enderlook.EventManager/src/SpinLockSlim.cs
+++ Enderlook.EventManager/src/SpinLockSlim.cs
@@ -8,7 +8,7 @@ namespace Enderlook.EventManager;
 /// <summary>
 /// A simple non-reentrant lock.
 /// </summary>
-public struct SpinLock
+public struct SpinLockSlim
 {
     private volatile int acquired;
 
@@ -19,7 +19,7 @@ public struct SpinLock
     public bool IsAcquired
     {
 #if NET5_0_OR_GREATER
-    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
 #else
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
@@ -46,6 +46,25 @@ public struct SpinLock
     }
 
     /// <summary>
+    /// Enter the lock.
+    /// </summary>
+    /// <param name="taken">If the method returns, this will be always <see langword="true"/>.<br/>
+    /// If an exception ocurrs, the parameter will be asigned the value <see langword="true"/> only if the lock was taken.<br/>
+    /// Otherwise, no value is assigned.</param>
+    /// <param name="spinWait">Spinner used to wait between attempts of locking.</param>
+#if NET5_0_OR_GREATER
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+#else
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+    public void Enter(ref bool taken, ref SpinWait spinWait)
+    {
+        while (TryAcquire())
+            spinWait.SpinOnce();
+        taken = true;
+    }
+
+    /// <summary>
     /// Try enter the lock if it's not already acquired.
     /// </summary>
     /// <param name="taken">If the method returns, this determines if the lock was taken.<br/>
@@ -60,22 +79,21 @@ public struct SpinLock
     /// <summary>
     /// Try enter the lock if it's not already acquired.
     /// </summary>
-    /// <param name="iterations">The number of attempts to acquire the lock before returning without it.</param>
     /// <param name="taken">If the method returns, this determines if the lock was taken.<br/>
     /// If an exception ocurrs, the parameter will be asigned the value <see langword="true"/> only if the lock was taken, otherwise, no value is assigned.</param>
+    /// <param name="iterations">The number of attempts to acquire the lock before returning without it.</param>
 #if NET5_0_OR_GREATER
     [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
 #else
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-    public void TryEnter(uint iterations, ref bool taken)
+    public void TryEnter(ref bool taken, uint iterations)
     {
         while (TryAcquire())
         {
-            if (unchecked(iterations--) == 0) // decrease after check so an initial value of 0 to `iterations` works.
-            {
+            // Decrease after check so an initial value of 0 to `iterations` works.
+            if (unchecked(iterations--) == 0)
                 return;
-            }
         }
         taken = true;
     }
@@ -83,24 +101,71 @@ public struct SpinLock
     /// <summary>
     /// Try enter the lock if it's not already acquired.
     /// </summary>
-    /// <param name="timeout">The <see cref="TimeSpan"/> to attempt to acquire the lock defore returning without it.<br/>
-    /// A negative <see cref="TimeSpan"/> causes undefined behaviour.</param>
     /// <param name="taken">If the method returns, this determines if the lock was taken.<br/>
     /// If an exception ocurrs, the parameter will be asigned the value <see langword="true"/> only if the lock was taken, otherwise, no value is assigned.</param>
+    /// <param name="iterations">The number of attempts to acquire the lock before returning without it.</param>
+    /// <param name="spinWait">Spinner used to wait between attempts of locking.</param>
 #if NET5_0_OR_GREATER
     [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
 #else
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-    public void TryEnter(TimeSpan timeout, ref bool taken)
+    public void TryEnter(ref bool taken, uint iterations, ref SpinWait spinWait)
+    {
+        while (TryAcquire())
+        {
+            // Decrease after check so an initial value of 0 to `iterations` works.
+            if (unchecked(iterations--) == 0)
+                return;
+            spinWait.SpinOnce();
+        }
+        taken = true;
+    }
+
+    /// <summary>
+    /// Try enter the lock if it's not already acquired.
+    /// </summary>
+    /// <param name="taken">If the method returns, this determines if the lock was taken.<br/>
+    /// If an exception ocurrs, the parameter will be asigned the value <see langword="true"/> only if the lock was taken, otherwise, no value is assigned.</param>
+    /// <param name="timeout">The <see cref="TimeSpan"/> to attempt to acquire the lock defore returning without it.<br/>
+    /// A negative <see cref="TimeSpan"/> causes undefined behaviour.</param>
+#if NET5_0_OR_GREATER
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+#else
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+    public void TryEnter(ref bool taken, TimeSpan timeout)
     {
         long end = unchecked((long)timeout.TotalMilliseconds * Stopwatch.Frequency + Stopwatch.GetTimestamp());
         while (TryAcquire())
         {
             if (Stopwatch.GetTimestamp() >= end)
-            {
                 return;
-            }
+        }
+        taken = true;
+    }
+
+    /// <summary>
+    /// Try enter the lock if it's not already acquired.
+    /// </summary>
+    /// <param name="taken">If the method returns, this determines if the lock was taken.<br/>
+    /// If an exception ocurrs, the parameter will be asigned the value <see langword="true"/> only if the lock was taken, otherwise, no value is assigned.</param>
+    /// <param name="timeout">The <see cref="TimeSpan"/> to attempt to acquire the lock defore returning without it.<br/>
+    /// A negative <see cref="TimeSpan"/> causes undefined behaviour.</param>
+    /// <param name="spinWait">Spinner used to wait between attempts of locking.</param>
+#if NET5_0_OR_GREATER
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+#else
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+    public void TryEnter(ref bool taken, TimeSpan timeout, ref SpinWait spinWait)
+    {
+        long end = unchecked((long)timeout.TotalMilliseconds * Stopwatch.Frequency + Stopwatch.GetTimestamp());
+        while (TryAcquire())
+        {
+            if (Stopwatch.GetTimestamp() >= end)
+                return;
+            spinWait.SpinOnce();
         }
         taken = true;
     }
